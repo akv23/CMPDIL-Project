d7bc15f23d40db8d8176381b643aefcd8713b22a
Aman Kumar <aman712ak@gmail.com>
2022-05-27 00:41:51 +0530

    update headbar, footer, sidebar,dashboard

--- admin/templates/footer.php
+++ admin/templates/footer.php
@@ -1,10 +1,6 @@
   <!-- ======= Footer ======= -->
   <footer id="footer" class="footer">
     <div class="copyright">
-      &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
-    </div>
-    <div class="credits">
-      
-      Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      &copy; Copyright <strong><span>Admin</span></strong>. All Rights Reserved
     </div>
   </footer><!-- End Footer -->
