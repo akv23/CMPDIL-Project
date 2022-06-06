2d53fe8b31d9ea2ade1e7d97b438f5aa0626a5b7
Sakshi Swati <sakshiswati33@gmail.com>
2022-06-06 19:09:18 +0530

    changes

--- admin/DB/cmpdi.sql
+++ admin/DB/cmpdi.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 18, 2022 at 06:51 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Generation Time: Jun 06, 2022 at 03:35 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,56 @@ SET time_zone = "+00:00";
 --
 -- Database: `cmpdi`
 --
+CREATE DATABASE IF NOT EXISTS `cmpdi` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `cmpdi`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `designation`
+--
+
+DROP TABLE IF EXISTS `designation`;
+CREATE TABLE `designation` (
+  `designation_id` int(10) NOT NULL,
+  `designation` varchar(50) NOT NULL,
+  `role` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `designation`
+--
+
+INSERT INTO `designation` (`designation_id`, `designation`, `role`) VALUES
+(1, 'HOD', 'HEAD OF DEPARTMENT'),
+(2, 'GM', 'GENERAL MANAGER'),
+(3, 'CM', 'CHIEF MANAGER'),
+(4, 'SM', 'SENIOR MANAGER'),
+(5, 'M', 'MANAGER'),
+(6, 'DM', 'DEPUTY MANAGER'),
+(7, 'AM', 'ASSISTANT MANAGER'),
+(8, 'MT', 'MANAGEMENT TRAINEE');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+CREATE TABLE `employee` (
+  `emp_id` int(16) NOT NULL,
+  `emp_name` varchar(50) NOT NULL,
+  `designation` varchar(50) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `mob_no` int(14) NOT NULL,
+  `create_date` date NOT NULL DEFAULT current_timestamp(),
+  `update_date` date NOT NULL DEFAULT current_timestamp(),
+  `status` varchar(8) NOT NULL,
+  `admin` varchar(4) NOT NULL,
+  `last_login_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -46,10 +96,38 @@ INSERT INTO `tbl_portal_admin` (`userid`, `password`, `status`) VALUES
 --
 
 --
+-- Indexes for table `designation`
+--
+ALTER TABLE `designation`
+  ADD PRIMARY KEY (`designation_id`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`emp_id`);
+
+--
 -- Indexes for table `tbl_portal_admin`
 --
 ALTER TABLE `tbl_portal_admin`
   ADD PRIMARY KEY (`userid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `designation`
+--
+ALTER TABLE `designation`
+  MODIFY `designation_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `emp_id` int(16) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
