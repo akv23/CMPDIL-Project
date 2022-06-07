cbc9c20438cceb0ea5544cbe17991b4d59fe126d
Aman Kumar <aman712ak@gmail.com>
2022-06-07 21:20:26 +0530

    modified insert

--- admin/insert.php
+++ admin/insert.php
@@ -1,12 +1,12 @@
 <?php
 if(isset($_POST['submit'])){ // Fetching variables of the form which travels in URL
-$connection = mysql_connect("localhost", "root", ""); // Establishing Connection with Server
-$db = mysql_select_db("cmpdi", $connection); // Selecting Database from Server
+$connection = mysqli_connect("localhost", "root", "","cmpdi"); // Establishing Connection with Server
+$db = mysqli_select_db( $connection,"cmpdi"); // Selecting Database from Server
 
 $name = $_POST['firstName'];
 $mobile = $_POST['Mob_no'];
 $Designation = $_POST['designation'];
-$query = mysql_query("insert into employee(emp_name ) values ('$name', '$mobile', '$Designation')");
+$sql= "insert into employee values ('$name', '$mobile', '$Designation')";
 if (mysqli_query($connection, $query)){
 
 
