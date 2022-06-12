b7489c488c013287526128d8d86f8c881ed184d9
Aman Kumar <aman712ak@gmail.com>
2022-06-12 15:15:33 +0530

    profile page

--- admin/profile.php
+++ admin/profile.php
@@ -61,12 +61,6 @@ $rs=mysqli_fetch_assoc($smt);
             <div class="container emp-profile shadow p-3 mb-5 bg-body" style="border-radius: 0.5rem;">
                 <form method="post">
                     <div class="row">
-                        <div class="col-md-4">
-                            <div class="profile-img">
-                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog" alt="" />
-    
-                            </div>
-                        </div>
                         <?php 
                                     include "./conn.php";
 									
@@ -77,72 +71,61 @@ $rs=mysqli_fetch_assoc($smt);
                                     while($rs=mysqli_fetch_assoc($smt))
                                     {
              
-                                        echo '<div class="col-md-6">';
-                                        echo '<div class="profile-head">';
-                                        echo "<h5>" .$rs['emp_id'].  "</h5>";
-                                   echo "<h6>";
-                                   echo " Head Manager";
-                                   echo " </h6>";
-                                   echo '<p class="proile-rating">"; YEAR EXPERIENCE : <span>";10</span>";</p>';
-                                   echo  '<ul class="nav nav-tabs" id="myTab" role="tablist">';
-                                   echo '<li class="nav-item">';
-                                   echo '<a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">About</a>';
-                                   echo " </li>";
-                                   
-                                   echo " </ul>";
-                                   echo " </div>";
-                                   echo "</div>";
-                        
-                                   echo " </div>";
-                                   echo '<div class="row">';
-                        echo '<div class="col-md-4">';
-                            
-                       echo" </div>";
-                        echo '<div class="col-md-8">';
-                            echo '<div class="tab-content profile-tab" id="myTabContent">';
-                                echo '<div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">';
-                                    echo '<div class="row">';
-                                        echo '<div class="col-md-6">';
-                                        echo " <label>admin id</label>";
-                                        echo"</div>";
-                                        echo '<div class="col-md-6">' .$rs['emp_id']. '</div>';
-                                          echo " </div>";
-                                    echo '<div class="row">';
-                                        echo '<div class="col-md-6">';
-                                        echo '<label>Name</label>';
-                                            echo " </div>";
-                                        echo '<div class="col-md-6">';
-                                        echo '<p> ADMIN</p>';
-                                        echo "  </div>";
-                                        echo " </div>";
-                                    echo '<div class="row">';
-                                        echo '<div class="col-md-6">';
-                                        echo "  <label>password</label>";
-                                        echo " </div>";
-                                        echo '<div class="col-md-6 '.$rs['password']. ' </div>';
-                                      echo "</div>";
-                                    echo '<div class="row">';
-                                        echo '<div class="col-md-6">';
-                                        echo "<label>Phone</label>";
-                                        echo " </div>";
-                                        echo '<div class="col-md-6">';
-                                        echo " <p>123 456 7890</p>";
-                                        echo "</div>";
-                                        echo " </div>";
-                                    echo '<div class="row">';
-                                        echo '<div class="col-md-6">;
-                                            <label>";Designation</label>";
-                                        </div>';
-                                        echo '<div class="col-md-6">';
-                                        echo ' <p>Head Manager</p>';
-                                        echo ' </div>';
-                                        echo ' </div>';
-                                        echo '</div>';
-                                        echo ' </div>';
-                                        echo ' </div>';
-                                        echo ' </div>';
-                                        echo ' </form>';
-                                        echo ' </div>';
+                                        echo '<div class="col-md-6">
+                                        <div class="profile-head">
+                                            <h5> '.$rs['emp_id']. '</h5>
+                                            <h6>Head Manager</h6>
+                                            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                                                <li class="nav-item">
+                                                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">About</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                        </div>
+                                        <div class="col-md-8">
+                                            <div class="tab-content profile-tab" id="myTabContent">
+                                                <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                                                    <div class="row">
+                                                        <div class="col-md-6">
+                                                            <label>admin id</label>
+                                                        </div>
+                                                        <div class="col-md-6">'.$rs['emp_id']. '</div>
+                                                    </div>
+                                                    <div class="row">
+                                                        <div class="col-md-6">
+                                                            <label>Name</label>
+                                                        </div>
+                                                        <div class="col-md-6">
+                                                            <p> ADMIN</p>
+                                                        </div>
+                                                    </div>
+                                                    
+                                                    <div class="row">
+                                                        <div class="col-md-6">
+                                                            <label>Phone</label>
+                                                        </div>
+                                                        <div class="col-md-6">
+                                                            <p>123 456 7890</p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row">
+                                                        <div class="col-md-6">
+                                                            <label>Designation</label>
+                                                        </div>
+                                                        <div class="col-md-6">
+                                                            <p>Head Manager</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    </form>
+                                    </div>';
                                     };
                                     ?>
 
