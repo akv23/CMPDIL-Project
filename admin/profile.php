73ea5a33064f39e1d438938a95f0848a0d0e6eba
Aman Kumar <aman712ak@gmail.com>
2022-06-12 14:19:51 +0530

    update profile page

--- admin/profile.php
+++ admin/profile.php
@@ -77,18 +77,18 @@ $rs=mysqli_fetch_assoc($smt);
                                     while($rs=mysqli_fetch_assoc($smt))
                                     {
              
-                                        echo "<div class="col-md-6">";
-                                        echo "<div class="profile-head">";
+                                        echo '<div class="col-md-6">';
+                                        echo '<div class="profile-head">';
                                         echo "<h5>";
-                                   echo " .$rs['userid']."
+                                   echo " .$rs['userid'].";
                                    echo "</h5>";
                                    echo "<h6>";
-                                   echo " Head Manager"
+                                   echo " Head Manager";
                                    echo " </h6>";
-                                   echo "<p class="proile-rating">"; YEAR EXPERIENCE : <span>";10</span>";</p>";
-                                   echo " <ul class="nav nav-tabs" id="myTab" role="tablist">";
-                                   echo " <li class="nav-item">";
-                                   echo "  <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">About</a>";
+                                   echo '<p class="proile-rating">"; YEAR EXPERIENCE : <span>";10</span>";</p>';
+                                   echo  '<ul class="nav nav-tabs" id="myTab" role="tablist">';
+                                   echo '<li class="nav-item">';
+                                   echo '<a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">About</a>';
                                    echo " </li>";
                                    
                                    echo " </ul>";
@@ -96,59 +96,59 @@ $rs=mysqli_fetch_assoc($smt);
                                    echo "</div>";
                         
                                    echo " </div>";
-                                   echo " <div class="row">";
-                        echo "<div class="col-md-4">";
+                                   echo '<div class="row">';
+                        echo '<div class="col-md-4">';
                             
                        echo" </div>";
-                        echo "<div class="col-md-8">";
-                            echo "<div class="tab-content profile-tab" id="myTabContent">";
-                                echo "<div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">";
-                                    echo "<div class="row">";
-                                        echo "<div class="col-md-6">";
+                        echo '<div class="col-md-8">';
+                            echo '<div class="tab-content profile-tab" id="myTabContent">';
+                                echo '<div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">';
+                                    echo '<div class="row">';
+                                        echo '<div class="col-md-6">';
                                         echo " <label>admin id</label>";
-                                        </div>";
-                                        echo "<div class="col-md-6">";
-                                          echo ".$rs['userid']." 
+                                        echo"</div>";
+                                        echo '<div class="col-md-6">';
+                                          echo ".$rs['userid']." ;
+                                          echo '</div>';
                                           echo " </div>";
-                                          echo " </div>";
-                                    echo "<div class="row">";
-                                        echo "<div class="col-md-6">";
-                                        echo "  <label>Name</label>";
+                                    echo '<div class="row">';
+                                        echo '<div class="col-md-6">';
+                                        echo '<label>Name</label>';
                                             echo " </div>";
-                                        echo "<div class="col-md-6">";
-                                        echo " <p> ADMIN</p>";
+                                        echo '<div class="col-md-6">';
+                                        echo '<p> ADMIN</p>';
                                         echo "  </div>";
                                         echo " </div>";
-                                    echo "<div class="row">";
-                                        echo "<div class="col-md-6">";
+                                    echo '<div class="row">';
+                                        echo '<div class="col-md-6">';
                                         echo "  <label>password</label>";
                                         echo " </div>";
-                                        echo "<div class="col-md-6">";
-                                      echo "  .$rs['password']."
+                                        echo '<div class="col-md-6">';
+                                      echo "  .$rs['password'].";
                                       echo "  </div>";
                                       echo "</div>";
-                                    echo "<div class="row">";
-                                        echo "<div class="col-md-6">";
+                                    echo '<div class="row">';
+                                        echo '<div class="col-md-6">';
                                         echo "<label>Phone</label>";
                                         echo " </div>";
-                                        echo "<div class="col-md-6">";
+                                        echo '<div class="col-md-6">';
                                         echo " <p>123 456 7890</p>";
                                         echo "</div>";
                                         echo " </div>";
-                                    echo "<div class="row">";
-                                        echo "<div class="col-md-6">";
+                                    echo '<div class="row">';
+                                        echo '<div class="col-md-6">;
                                             <label>";Designation</label>";
-                                        </div>";
-                                        echo "<div class="col-md-6">";
-                                        echo " <p>Head Manager</p>";
-                                        echo " </div>";
-                                        echo " </div>";
-                                        echo "</div>";
-                                        echo " </div>";
-                                        echo " </div>";
-                                        echo " </div>";
-                                        echo " </form>";
-                                        echo " </div>";
+                                        </div>';
+                                        echo '<div class="col-md-6">';
+                                        echo ' <p>Head Manager</p>';
+                                        echo ' </div>';
+                                        echo ' </div>';
+                                        echo '</div>';
+                                        echo ' </div>';
+                                        echo ' </div>';
+                                        echo ' </div>';
+                                        echo ' </form>';
+                                        echo ' </div>';
                                     };
                                     ?>
 
